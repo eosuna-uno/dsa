460a825d09661b5acf898b184b5a88f11b2eb514
Ernesto Osuna <ernesto.osuna@unosquare.com>
2025-02-13 12:45:23 -0600

    add fibbonaci sequence

---
+++ Recursion/Exercises_easy/fib/solution.ts
@@ -0,0 +1,17 @@
+import { assertEquals } from "jsr:@std/assert/equals";
+
+function fib(num: number): number {
+  if (num <= 0) return 0;
+  if (num == 1) return 1;
+  if (num == 2) return 1;
+  return fib(num - 1) + fib(num - 2);
+}
+Deno.test({
+  name: "product of array",
+  fn: () => {
+    assertEquals(fib(4), 3);
+    assertEquals(fib(10), 55);
+    assertEquals(fib(28), 317811);
+    assertEquals(fib(35), 9227465);
+  },
+});
